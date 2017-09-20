3a8f274b0925180e01940a3abe73ea1e6305cd00
davidlhayes <david@davidhayes.us>
2017-09-20 09:22:38 -0500

    added text to output

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // When you click in the body...
+
 $('body').on('click', function () {
     console.log("app.js");
     // Display a sum of random values from random.js
     var startValue = 0;
     var newValue = startValue + sumRandoms(10);
-    $('#output').text(newValue);
+    $('#output').text("The answer is" + newValue);
 });
