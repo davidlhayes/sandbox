26719c288d83ff3d527d5d955e9dd1d7de66d7c3
davidlhayes <david@davidhayes.us>
2017-09-20 10:48:59 -0500

    stuff

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ $('body').on('click', function () {
     console.log("app.js");
     // Display a sum of random values from random.js
     var startValue = 0;
-    var newValue = startValue + sumRandoms(10);
+    var newValue = startValue + 30 * sumRandoms(10);
     $('#time').text(moment().format("MM/DD/YY @ HH:mm"));
-    $('#output').text(newValue);
+    $('#answer').text(newValue);
 });
